cd63c7d7b92b3f89a028aaed517e76daa9d5f780
Joshua Armoney <armoneyj210@gmail.com>
2019-10-31 12:54:09 -0400

    created game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var card1 = "queen"
+var card2 = "king"
+var card3 = "king"
+var card4 = "queen"
+console.log("User flipped " + card1);
+console.log("User flipped " + card2);
