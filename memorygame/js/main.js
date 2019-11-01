b5ef8007b9a9becf128e3c5c6dc9f8c84e342e54
Joshua Armoney <armoneyj210@gmail.com>
2019-11-01 14:25:25 -0400

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,15 +1,19 @@
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
-const cardOne = [0];
-cardsInPlay.push(cardOne);
-console.log("user flipped a queen");
-const cardTwo = [2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped a king");
-if (cardsInPlay.length === 2);
-else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay.length === 2);
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
 }
-else (cardsInPlay[0] !== cardsInPlay[1]) 
-	alert("Sorry try again.");
-
+else { 
+	console.log("Sorry try again.");
+}
+}
+function flipCard(cardId){
+	
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
